53c820958eb1d61d3d54f4abd82e31f91c8207f7
shoxjahon527 <134792325+shoxjahon527@users.noreply.github.com>
2023-12-22 21:17:58 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,148 @@
+let aded  = document.querySelector('.pseta4')
+let aded1  = document.querySelector('.pseta3')
+let aded2  = document.querySelector('.pseta2')
+let aded3  = document.querySelector('.pseta1')
+let img4 = document.querySelector('.itemfg1')
+let img3 = document.querySelector('.itemfg2')
+let img2 = document.querySelector('.itemfg3')
+let img1 = document.querySelector('.itemfg4')
+function iteb4() {
+    if (aded2.classList.contains('yoniq')) {
+        aded2.classList.remove('yoniq')
+        aded2.innerHTML = ``
+    img3.setAttribute('src', 'ph_plus-circle-fill.svg')
+    }
+    if (aded1.classList.contains('yoniq')) {
+        aded1.classList.remove('yoniq')
+        aded1.innerHTML = ``
+    img2.setAttribute('src', 'ph_plus-circle-fill.svg')
+    }
+    if (aded.classList.contains('yoniq')) {
+        aded.classList.remove('yoniq')
+        aded.innerHTML = ``
+    img1.setAttribute('src', 'ph_plus-circle-fill.svg')
+    }
+    if (aded3.classList.contains('yoniq')) {
+        aded3.classList.remove('yoniq')
+        aded3.innerHTML = 
+    `
+      
+    `
+    img4.setAttribute('src', 'ph_plus-circle-fill.svg')
+    }else{
+    aded3.classList.add('yoniq')
+    aded3.innerHTML = 
+    `
+    <p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi possimus quidem ullam, nisi quos iste quaerat unde tenetur dolorem dolore repudiandae quibusdam magnam alias enim atque facilis voluptatum, maxime quis.
+
+        </p>
+    `
+    img4.setAttribute('src', 'ph_minus-circle-fill.svg')
+    }
+}
+function iteb3() {
+    if (aded3.classList.contains('yoniq')) {
+        aded3.classList.remove('yoniq')
+        aded3.innerHTML = ``
+    img4.setAttribute('src', 'ph_plus-circle-fill.svg')
+    }
+    if (aded1.classList.contains('yoniq')) {
+        aded1.classList.remove('yoniq')
+        aded1.innerHTML = ``
+    img2.setAttribute('src', 'ph_plus-circle-fill.svg')
+    }
+    if (aded.classList.contains('yoniq')) {
+        aded.classList.remove('yoniq')
+        aded.innerHTML = ``
+    img1.setAttribute('src', 'ph_plus-circle-fill.svg')
+    }
+    if (aded2.classList.contains('yoniq')) {
+        aded2.classList.remove('yoniq')
+        aded2.innerHTML = 
+    `
+     
+    `
+    img3.setAttribute('src', 'ph_plus-circle-fill.svg')
+    }else{
+    aded2.classList.add('yoniq')
+    aded2.innerHTML = 
+    `
+    <p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi possimus quidem ullam, nisi quos iste quaerat unde tenetur dolorem dolore repudiandae quibusdam magnam alias enim atque facilis voluptatum, maxime quis.
+
+        </p>
+    `
+    img3.setAttribute('src', 'ph_minus-circle-fill.svg')
+    }
+}
+function iteb2() {
+    if (aded3.classList.contains('yoniq')) {
+        aded3.classList.remove('yoniq')
+        aded3.innerHTML = ``
+    img4.setAttribute('src', 'ph_plus-circle-fill.svg')
+    }
+    if (aded2.classList.contains('yoniq')) {
+        aded2.classList.remove('yoniq')
+        aded2.innerHTML = ``
+    img3.setAttribute('src', 'ph_plus-circle-fill.svg')
+    }
+    if (aded.classList.contains('yoniq')) {
+        aded.classList.remove('yoniq')
+        aded.innerHTML = ``
+    img1.setAttribute('src', 'ph_plus-circle-fill.svg')
+    }
+    if (aded1.classList.contains('yoniq')) {
+        aded1.classList.remove('yoniq')
+        aded1.innerHTML = 
+    `
+      
+    `
+    img2.setAttribute('src', 'ph_plus-circle-fill.svg')
+    }else{
+    aded1.classList.add('yoniq')
+    aded1.innerHTML = 
+    `
+    <p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi possimus quidem ullam, nisi quos iste quaerat unde tenetur dolorem dolore repudiandae quibusdam magnam alias enim atque facilis voluptatum, maxime quis.
+
+        </p>
+    `
+    img2.setAttribute('src', 'ph_minus-circle-fill.svg')
+    }
+}
+function iteb1() {
+    if (aded3.classList.contains('yoniq')) {
+        aded3.classList.remove('yoniq')
+        aded3.innerHTML = ``
+    img4.setAttribute('src', 'ph_plus-circle-fill.svg')
+    }
+    if (aded1.classList.contains('yoniq')) {
+        aded1.classList.remove('yoniq')
+        aded1.innerHTML = ``
+    img2.setAttribute('src', 'ph_plus-circle-fill.svg')
+    }
+    if (aded.classList.contains('yoniq')) {
+        aded2.classList.remove('yoniq')
+        aded2.innerHTML = ``
+    img3.setAttribute('src', 'ph_plus-circle-fill.svg')
+    }
+    if (aded.classList.contains('yoniq')) {
+        aded.classList.remove('yoniq')
+        aded.innerHTML = 
+    `
+      
+    `
+    img1.setAttribute('src', 'ph_plus-circle-fill.svg')
+    }else{
+    aded.classList.add('yoniq')
+    aded.innerHTML = 
+    `
+    <p>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi possimus quidem ullam, nisi quos iste quaerat unde tenetur dolorem dolore repudiandae quibusdam magnam alias enim atque facilis voluptatum, maxime quis.
+
+        </p>
+    `
+    img1.setAttribute('src', 'ph_minus-circle-fill.svg')
+    }
+}
